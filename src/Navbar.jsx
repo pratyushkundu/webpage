687574874e8185b6406c18bbbe5d92fadc50f77a
prat <pratyushkundu123@gmail.com>
2021-12-16 13:08:09 +0530

    test2

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,27 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-const Navbar = ()=>{
-    return(<> <div className="nav_bar">
-    <NavLink to="/" exact activeClassName="active_class" style={{textDecoration:"none"}}> <h2 className="nav_hover" >Home</h2></NavLink>
-    <NavLink to ="/about"  exact activeClassName="active_class" style={{textDecoration:"none"}}> <h2 className="nav_hover" >AboutSociety</h2></NavLink>
-    <NavLink to="/members" exact activeClassName="active_class" style={{textDecoration:"none"}}> <h2 className="nav_hover" >Members</h2></NavLink> 
-    <NavLink to="/contact" exact activeClassName="active_class" style={{textDecoration:"none"}}> <h2 className="nav_hover" >ContactUs</h2></NavLink> 
+const navbar = () => {
+    const w = 700;
+    if (window.screen.width <= 700) {
+        return (<>
+            <NavLink to="/" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >Home</h2></NavLink>
+            <NavLink to="/about" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >AboutSociety</h2></NavLink>
+            {/* <NavLink to="/members" exact activeClassName="active_class" style={{textDecoration:"none"}}> <h2 className="nav_hover" >Members</h2></NavLink>  */}
+            <NavLink to="/contact" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >ContactUs</h2></NavLink>
+        </>)
+    }
+    else {
+        return (<>
+            <NavLink to="/" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >Home</h2></NavLink>
+            <NavLink to="/about" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >AboutSociety</h2></NavLink>
+            <NavLink to="/members" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >Members</h2></NavLink>
+            <NavLink to="/contact" exact activeClassName="active_class" style={{ textDecoration: "none" }}> <h2 className="nav_hover" >ContactUs</h2></NavLink>
+        </>)
+    }
+}
+const Navbar = () => {
+    return (<> <div className="nav_bar">
+        {navbar()}
     </div></>)
 }
 export default Navbar;
