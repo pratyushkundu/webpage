6a06aa331c63df1c55a5dceda9a18c47b7588568
prat <pratyushkundu123@gmail.com>
2021-12-26 16:21:30 +0530

    test6

--- src/Apps.jsx
+++ src/Apps.jsx
@@ -434,165 +434,45 @@ const Apps = () => {
                         <h3 style={{ display: "flex", flexDirection: "column" }}><span>xyzzzzz:92732709727</span><span>chjsgddjsg:792347979</span></h3>
                     </div>
                 </div> */}
-                {/* <div class="container my-5">
-  <footer
-          class="text-center text-lg-start text-white"
-          style={{backgroundColor:" #45526e"}}
-          >
-    <div class="container p-4 pb-0">
-      <section class="">
-        <div class="row">
-          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">
-              Company name
-            </h6>
-            <p>
-              Here you can use rows and columns to organize your footer
-              content. Lorem ipsum dolor sit amet, consectetur adipisicing
-              elit.
-            </p>
-          </div>
 
-          <hr class="w-100 clearfix d-md-none" />
 
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
-            <p>
-              <a class="text-white">MDBootstrap</a>
-            </p>
-            <p>
-              <a class="text-white">MDWordPress</a>
-            </p>
-            <p>
-              <a class="text-white">BrandFlow</a>
-            </p>
-            <p>
-              <a class="text-white">Bootstrap Angular</a>
-            </p>
-          </div>
-
-          <hr class="w-100 clearfix d-md-none" />
-
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">
-              Useful links
-            </h6>
-            <p>
-              <a class="text-white">Your Account</a>
-            </p>
-            <p>
-              <a class="text-white">Become an Affiliate</a>
-            </p>
-            <p>
-              <a class="text-white">Shipping Rates</a>
-            </p>
-            <p>
-              <a class="text-white">Help</a>
-            </p>
-          </div>
-
-          <hr class="w-100 clearfix d-md-none" />
-
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
-            <p><i class="fa fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fa fa-envelope mr-3"></i> info@gmail.com</p>
-            <p><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
-          </div>
-        </div>
-      </section>
-
-      <hr class="my-3"/>
-
-      <section class="p-3 pt-0">
-        <div class="row d-flex align-items-center">
-          <div class="col-md-7 col-lg-8 text-center text-md-start">
-            <div class="p-3">
-              © 2020 Copyright:
-              <a class="text-white" href="https://mdbootstrap.com/"
-                 >MDBootstrap.com</a
-                >
+                <footer>
+                    <div class="top_header">
+                        <section>
+                            <span><i class="fa fa-map-marker"></i></span>
+                            <span>NIT ROAD, Sector-2, Odisha, India, 769015</span>
+                        </section>
+                        <section>
+                            <span><i class="fa fa-phone"></i></span>
+                            <span>Siddharth Nanda 734597839</span>
+                            <span><i class="fa fa-phone"></i></span>
+                            <span>Sakti Swaroop 8936498379</span>
+                        </section>
+                        <section>
+                            <span><i class="fa fa-envelope"></i></span>
+                            <span>info@websitename.com</span>
+                        </section>
+                    </div>
+                    <span class="border-shape"></span>
+                    <div class="bottom_content">
+                        <section>
+                            <a href="#"><i class="fa fa-facebook" style={{ fontSize: "2rem" }}></i></a>
+                            <a href="#"><i class="fa fa-instagram" style={{ fontSize: "2rem" }}></i></a>
+                            <a href="#"><i class="fa fa-linkedin" style={{ fontSize: "2rem" }}></i></a>
+                            {/* <a href="#"><i class="fa fa-telegram"></i></a> */}
+                        </section>
+                        <section>
+                            <a href="#" style={{ fontSize: "1.1rem" }}>Home</a>
+                            <a href="#" style={{ fontSize: "1.1rem" }}>About</a>
+                            <a href="#" style={{ fontSize: "1.1rem" }}>Contact</a>
+                            <a href="#" style={{ fontSize: "1.1rem" }}>Members</a>
+                        </section>
+                    </div>
+                    <div class="copyright">
+                        Copyright © 2021 NITRICS - All rights reserved
+                    </div>
+                </footer>
             </div>
-          </div>
-
-          <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fa fa-facebook"></i
-              ></a>
-
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fa fa-twitter"></i
-              ></a>
-
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fa fa-google"></i
-              ></a>
-
-            <a
-               class="btn btn-outline-light btn-floating m-1"
-               class="text-white"
-               role="button"
-               ><i class="fa fa-instagram"></i
-              ></a>
-          </div>
-        </div>
-      </section>
-    </div>
-  </footer> */}
-                {/* // </div> */}
-                {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/> */}
-
-
-<footer>
-<div class="top_header">
-<section>
-<span><i class="fa fa-map-marker"></i></span>
-<span>NIT ROAD, Sector-2, Odisha, India, 769015</span>
-</section>
-<section>
-<span><i class="fa fa-phone"></i></span>
-<span>Siddharth Nanda 734597839</span>
-<span><i class="fa fa-phone"></i></span>
-<span>Sakti Swaroop 8936498379</span>
-</section>
-<section>
-<span><i class="fa fa-envelope"></i></span>
-<span>info@websitename.com</span>
-</section>
-</div>
-<span class="border-shape"></span>
-<div class="bottom_content">
-<section>
-<a href="#"><i class="fa fa-facebook" style={{fontSize:"2rem"}}></i></a>
-<a href="#"><i class="fa fa-instagram" style={{fontSize:"2rem"}}></i></a>
-<a href="#"><i class="fa fa-linkedin" style={{fontSize:"2rem"}}></i></a>
-{/* <a href="#"><i class="fa fa-telegram"></i></a> */}
-</section>
-<section>
-<a href="#" style={{fontSize:"1.3rem"}}>Home</a>
-<a href="#" style={{fontSize:"1.3rem"}}>About</a>
-<a href="#" style={{fontSize:"1.3rem"}}>Contact</a>
-<a href="#" style={{fontSize:"1.3rem"}}>Members</a>
-{/* <a href="#">Member</a> */}
-{/* <a href="#">Contact Us</a> */}
-</section>
-</div>
-<div class="copyright">
-Copyright © 2021 NITRICS - All rights reserved 
-</div>
-</footer>
-            {/* </footer> */}
-        </div>
         </div></>);
 }
 
